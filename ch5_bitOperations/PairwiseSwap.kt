9b4c975ac4f199827385a728bd38fa7343c34a2a
Ivan Esparza <ivan-esparza@morsum.co>
2019-12-30 17:39:48 -0700

    Uses Int instead of Long in 5.7

--- ch5_bitOperations/PairwiseSwap.kt
+++ ch5_bitOperations/PairwiseSwap.kt
@@ -1,13 +1,13 @@
 fun main(args: Array<String>) {
     if (args.size != 1) throw Exception("One argument must be provided: an Integer to be swapped")
-    val n = args[0].toLong()
+    val n = args[0].toInt()
     val swapped = pairwiseSwap(n)
     println("The number $n is $swapped when its pairs are swapped")
 }
 
-fun pairwiseSwap(n: Long): Long {
-    val evenMask = 0x555555555
-    val oddMask = 0xaaaaaaaa
+fun pairwiseSwap(n: Int): Int {
+    val evenMask = 0x55555555
+    val oddMask = evenMask.inv()
     val even = n and evenMask
     val odd = n and oddMask
     return even shl 1 or odd shr 1
